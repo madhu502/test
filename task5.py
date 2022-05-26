4ff3a9968e612a46357dae8faf4c687da3476668
madhu502 <chaudharymadhu502@gmail.com>
2022-05-26 19:14:36 +0545

    First commit

---
+++ task5.py
@@ -0,0 +1,178 @@
+#1
+
+# print('minimum is',min (1,2,3,4,5,6))
+
+##2
+# a=['Rishav',4,'Lion',4,8,2.3,'neha']
+# b=[]
+# if len(b)==0:
+#     print('list is empty.')
+# else:
+#     print('list is not empty.' )
+
+
+
+
+
+###3
+# import random 
+# list1=[1,2,3,4,5,6]
+# print(random.choice(list1))
+
+######4
+# data=["Raj","Simran",29]
+# data2=[]
+# data2=data.copy()
+# print(data)
+# print(data2)
+
+##5
+# list1 = [10, 20, 48, 45, 99]
+# list1.sort()
+# print("Second largest element is:", list1[-2])
+
+
+###6
+# data=["Mrinal","Rahul",'Bhumi','Upasna']
+# print(data)
+# data.remove('Bhumi')
+# print(data)
+
+###7
+# numbers=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17)
+# count_odd=0
+# count_even=0
+# for i in numbers:
+#     if i%2:
+#         count_even +=1
+#     else:
+#         count_odd +=1
+# print('Number of even numbers:',count_even)
+# print('Number of odd numbers:',count_odd)
+
+##8
+# data={1,2,3,4}
+# data.add(5)
+# print(data)
+
+###9
+# Tuple1=("Divya","Varun","Pratik")
+# print(Tuple1)
+# List1=list(Tuple1)
+# print(List1)
+
+##10
+# def convertTuple(tup):
+#     str=''.join(tup)
+#     return str
+# tuple=('p','y','t','h','o','n')
+# str=convertTuple(tuple)
+# print(str)
+
+##11
+# atuple=('Book','Library',25,'College')
+# alist=list(atuple)
+# print(type(alist))
+# print(alist)
+
+##12
+# a=[(1,'Sweta'),(2,'Shristhi'),(3,'Abhay')]
+# b=dict(a)
+# print(b)
+
+##13
+# a={0:10,1:20}
+# print(a)
+# a[2]=30
+# print(a)
+
+###14
+# dic1={1:10,2:20}
+# dic2={3:30,4:40}
+# dic3={5:50,6:60}
+# dic1.update(dic2)
+# dic1.update(dic3)
+# print(dic1)
+
+##15
+# data={'name':'ram','age':10}
+# if 'name' in data:
+#     print('present')
+# else:
+#     print('not present')
+
+##16
+# a=dict()
+# for i in range(1,16):
+#     a[i]=i**2
+# print(a)
+
+##17
+# def Merge(dict1,dict2):
+#     return(dict1.update(dict2))
+# dict1 = {'a':14,'b':45}
+# dict2 = {'c':52,'d':74}
+# print(Merge(dict1,dict2))
+# print(dict1)
+
+###18
+a=[55,64,78,95,21,47,58,49,75]
+print(sorted(a)[-3])
+
+
+
+
+
+###19
+# s1 ={4,7,5,8}
+# print(s1)
+# print(type(s1))
+
+###20
+# s=set('loNdOn')
+# for i in s:
+#     print(i)
+
+##21
+# data={1,2,3,4}
+# data.add(5)
+# print(data)
+
+##22
+# data={15,25,35,41,45}
+# data.remove(41)
+# print(data)
+
+## 23
+
+
+##24
+# a={1,2,3,4}
+# b={3,4,5,6}
+# intersection_set= a & b
+# print(intersection_set)
+
+##25
+# fruits={'a':'apple','b':'berry','c':'mango'}
+# key_to_lookup ='b'
+# if key_to_lookup in (fruits):
+#     print('key exists')
+# else:
+#     print('key doesnot exists')
+    
+
+###26
+# data={1:'a',2:'b',3:'c'}
+# if 'b' in data.values():
+#     print('present')
+# else:
+#     print('not present')
+
+
+
+
+
+
+
+
+
